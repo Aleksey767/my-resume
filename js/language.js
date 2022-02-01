5d9a32b18897592f7c2a181aff36341c5cf6d68e
Aleksey767 <98593351+Aleksey767@users.noreply.github.com>
2022-02-02 00:28:53 +0300

    add ru lang

---
+++ js/language.js
@@ -0,0 +1,167 @@
+const langArr = {
+
+    "title": {
+        "EN": "Aleksey Sazonov",
+        "RU": "Алексей Сазонов"
+    },
+    "greetings": {
+        "EN": "<span class=\"lng-greetings\">Hi there,i'm Aleksey.</span> I build interactive web apps that run across platforms & devices",
+        "RU": "<span class=\"lng-greetings\">Привет, я Алексей.</span> Я создаю интерактивные веб-приложения, которые работают на разных платформах и устройствах."
+    },
+    "button": {
+        "EN": "GET IN TOUCH",
+        "RU": "СВЯЗАТЬСЯ"
+    },
+    "first-text": {
+        "EN": "My name is Alexey Sazonov. I am beginner front-end developer with a lot of enthusiasm and attention to detail. I love making complex things simple and having fun using them. I'm currently focused on building Single Page Applications with React.",
+        "RU": "Меня зовут Алексей Сазонов. Я начинающий фронтенд-разработчик с большим энтузиазмом и вниманием к деталям. Я люблю делать сложные вещи простыми и получать удовольствие от их использования. В настоящее время я сосредоточен на создании одностраничных приложений с помощью React."
+    },
+    "second-text": {
+        "EN": "At the university, I dealt mainly with programming languages, with constructions close to machine instructions. Since then, I have been enjoying UI effects, animations and creating intuitive, dynamic user experience.",
+        "RU": "В университете я имел дело преимущественно с языками программирования конструкции которых близки к машинным инструкциями. С тех пор я получаю истинное удовольствие от UI эффектов , анимаций, создания интуитивно понятного, динамичного пользовательского интерфейса"
+    },
+    "title-skills": {
+        "EN": "Technical skill set",
+        "RU": "Навыки"
+    },
+    "subtitle-skills": {
+        "EN": "I’m constantly learning and keep up to date with the latest technologies, so I can pick the best tech for the job.",
+        "RU": "Я постоянно учусь и слежу за последними технологиями, поэтому я стараюсь выбирать подходящую из них для каждой работы."
+    },
+    "js": {
+        "EN": "It’s my main language and I know it well. I focus on building single-page apps with attention to code quality and best practices.",
+        "RU": "Это мой основной язык, и я хорошо его знаю. Я сосредоточен на создании одностраничных приложений, обращая внимание на качество кода и опыт передовых разработчиков в этой сфере."
+    },
+    "html": {
+        "EN": "I write semantic markup with accessibility in mind. I keep my CSS manageable and scalable strictly following BEM conventions.",
+        "RU": "Я пишу семантическую разметку с учетом доступности. Я держу свой CSS управляемым и масштабируемым, строго следуя соглашениям BEM."
+    },
+    "css": {
+        "EN": "Using CSS, I separately describe the logical structures of web pages. This allows you to increase the accessibility of the document, provide more flexibility and control over its presentation, and reduce complexity and repetitiveness in structured content. I've been using SASS a lot lately.",
+        "RU": "С помощью CSS я отдельно описываю логические структуры веб-страниц. Это позволяет повысить доступность документа, обеспечить большую гибкость и контроль над его представлением, а также уменьшить сложность и повторяемость структурированного содержимого. В последнее время я часто использую SASS."
+    },
+    "react": {
+        "EN": "It’s my main framework and i know it well. I use react to develop user interfaces as well as single page applications. React provides high speed, simplicity and scalability.",
+        "RU": "Это мой основной фреймворк, и я его хорошо знаю. Я использую React для разработки пользовательских интерфейсов, а также одностраничных приложений. React обеспечивает высокую скорость, простоту и масштабируемость."
+    },
+    "redux": {
+        "EN": "I am using Redux to manage the state of the application, in conjunction with the React framework. It helps in the development of the client side. Redux contains a number of tools to greatly simplify the transfer of storage data through the context.",
+        "RU": "Я использую Redux для управления состоянием приложения в сочетании с фреймворком React. Это помогает в развитии клиентской части. Redux содержит ряд инструментов, значительно упрощающих передачу данных хранилища через контекст."
+    },
+    "git": {
+        "EN": "Git is needed in order to manage versions of the application being developed. This allows me to conveniently merge and unmerge branches, as well as share my code with other developers.",
+        "RU": "Git нужен для того, чтобы управлять версиями разрабатываемого приложения. Это позволяет мне удобно объединять и разъединять ветки, а также делиться своим кодом с другими разработчиками."
+    },
+    "edu": {
+        "EN": "Education",
+        "RU": "Образование"
+    },
+    "place": {
+        "EN": "Branch of BSUIR MRC",
+        "RU": "Филиал БГУИР МРК"
+    },
+    "name": {
+        "EN": "Radio Technician",
+        "RU": "Радиотехник"
+    },
+    "dep": {
+        "EN": "Department of Radio Engineering",
+        "RU": "Кафедра радиотехники"
+    },
+    "desc": {
+        "EN": "Branch of BSUIR MRK is the oldest and most authoritative educational institution of the Republic of Belarus. The college annually graduates and accepts several hundred students from fields such as programming and electronics. The average passing score for admission to college exceeds more than 8.5 points, which makes it one of the most prestigious and popular in the country.",
+        "RU": "Филиал БГУИР МРК является старейшим и авторитетнейшим учебным заведением Республики Беларусь. Колледж ежегодно выпускает и принимает несколько сотен студентов из таких областей, как программирование и электроника. Средний проходной балл при поступлении в колледж превышает более 8,5 баллов, что делает его одним из самых престижных и популярных в стране."
+    },
+    "place2": {
+        "EN": "BSUIR",
+        "RU": "БГУИР"
+    },
+    "name2": {
+        "EN": "Systems Engineer",
+        "RU": "Инженер-системотехник"
+    },
+    "dep2": {
+        "EN": "Faculty of Computer Systems and Networks",
+        "RU": "Факультет Компьютерных Систем и Сетей"
+    },
+    "desc2": {
+        "EN": "BSUIR is one of the leading IT universities in the Republic of Belarus in training IT specialists. Today, about 16,000 students study at the university, 1,000 of which are foreign students The University closely cooperates with industry-leading IT companies - IBM, Microsoft, SAP, National Instruments, CISCO, NVidia, EMC, Texas Instruments, Oracle, Yandex, 1C. According to the results of a survey of HTP resident companies, FKSiS takes first place in the TOP-10 faculties of Belarusian universities, whose graduates are in demand in the High Technology Park.",
+        "RU": "БГУИР является одним из ведущих ИТ-вузов Республики Беларусь по подготовке ИТ-специалистов. На сегодняшний день в университете обучается около 16 000 студентов, 1000 из которых – иностранные студенты Университет тесно сотрудничает с ведущими отраслевыми ИТ-компаниями – IBM, Microsoft, SAP, National Instruments, CISCO, NVidia, EMC, Texas Instruments, Oracle, Яндекс, 1С . По результатам опроса компаний-резидентов ПВТ ФКСиС занимает первое место в ТОП-10 факультетов белорусских вузов, выпускники которых востребованы в Парке высоких технологий."
+    },
+    "yers": {
+        "EN": "Sept - Dec 2021",
+        "RU": "Сен - Дек 2021"
+    },
+    "desc3": {
+       "EN": "Wargaming Group Limited (also known as Wargaming.net) is a Belarusian video game company headquartered in Nicosia, Cyprus. The group operates across more than 20 offices globally and development studios, the largest of which is located in Minsk, where the company originated from. The company's games are played by millions of players around the world. The most famous of them are World of Warships and World of Tanks. More than 2000 specialists from different parts of the world work in Wargaming.",
+        "RU": "Wargaming Group Limited (также известная как Wargaming.net) — белорусская компания по производству видеоигр со штаб-квартирой в Никосии, Кипр. Группа работает в более чем 20 офисах и студиях разработки по всему миру, крупнейший из которых находится в Минске, откуда компания возникла. В игры компании играют миллионы игроков по всему миру. Самые известные из них — World of Warships и World of Tanks. В Wargaming работает более 2000 специалистов из разных уголков мира."
+    },
+    "work": {
+        "EN": "Some of my work",
+        "RU": "Некоторые из моих работ"
+    },
+    "works": {
+        "EN": "I care about code quality and building truly pleasant user experiences, with attention to small but important details.",
+        "RU": "Я забочусь о качестве кода и создании действительно приятного пользовательского интерфейса с вниманием к небольшим, но важным деталям."
+    },
+    "touch": {
+        "EN": "Get in touch",
+        "RU": "Связь со мной"
+    },
+    "hi": {
+        "EN": "Wanna talk to me about job, or just want to say hi? Feel free to email me at <span> <a class='mail' href='mailto:al_sazonov@list.ru'>al_sazonov@list.ru</a> </span> or use the form below.",
+        "RU": "У вас есть предложение, или просто хотите сказать привет? Пишите мне по адресу <span> <a class='mail' href='mailto:al_sazonov@list.ru'>al_sazonov@list.ru</a> </span> или используйте форму ниже."
+    },
+    "btn": {
+        "EN": "SEND MESSAGE",
+        "RU": "ОТПРАВИТЬ"
+    },
+    "footer": {
+        "EN": "Design and development would be Alexey Sazonov.",
+        "RU": "Дизайн и разработка — Алексея Сазонова."
+    },
+}
+const allLang = ['RU', 'EN']
+const select = document.getElementsByClassName('select');
+select[1].addEventListener('click', changeURLEng);
+select[0].addEventListener('click', changeURLRu);
+
+
+function changeURLRu() {
+
+    let lang = select[0].innerHTML
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+
+
+}
+
+function changeURLEng() {
+    let lang = select[1].innerHTML
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    if(location.href.includes('RU')){
+        const flag = document.querySelector('.flag');
+        const dropbtn = document.querySelector('.dropbtn');
+        flag.style.display=('block');
+        dropbtn.style.display=('none');
+    }
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#EN';
+        hash = select[1].innerHTML
+    }
+
+    document.querySelector('title').innerHTML = langArr['title'][hash];
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-' + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+    }
+}
+changeLanguage();
